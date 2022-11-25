f33d9f887519be827ae0679fe920e7c5861eb15f
himself949 <doubleh290986@hotmail.com>
2022-11-25 12:01:04 +0000

    add getInitialProps

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9d514bdd002783d0"; }
+/******/ 		__webpack_require__.h = function() { return "4c6e3fa84733ed42"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/blog.js
+++ pages/blog.js
@@ -2,20 +2,20 @@ import Link from "next/link"
 import posts from '../posts.json'
 
 const Blog = () => (
-    <div>
-        <h1>Blog</h1>
-        <ul>
-            {Object.entries(posts).map((value, index) => {
-                return (
-                    <li key={index}>
-                        <Link href='/blog/[id]' as={'/blog/' + value[0]}>
-                            {value[1].title}
-                        </Link>
-                    </li>
-                )
-            })}
-        </ul>
-    </div>
+  <div>
+    <h1>Blog</h1>
+    <ul>
+      {Object.entries(posts).map((value, index) => {
+        return (
+          <li key={index}>
+            <Link href='/blog/[id]' as={'/blog/' + value[0]}>
+              {value[1].title}
+            </Link>
+          </li>
+        )
+      })}
+    </ul>
+  </div>
 )
 
 export default Blog
